cce9e726a9797f2eb8b0d2a98732a4ed89da53cb
Gareth <gtrufitt@gmail.com>
2020-05-26 15:45:58 +0100

    Add initial ab test types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,71 @@
+export type OphanProduct =
+	| 'CONTRIBUTION'
+	| 'RECURRING_CONTRIBUTION'
+	| 'MEMBERSHIP_SUPPORTER'
+	| 'MEMBERSHIP_PATRON'
+	| 'MEMBERSHIP_PARTNER'
+	| 'DIGITAL_SUBSCRIPTION'
+	| 'PRINT_SUBSCRIPTION';
+
+export interface EngagementBannerTemplateParams {
+	titles?: Array<string>;
+	leadSentence?: string;
+	closingSentence?: string;
+	messageText: string;
+	mobileMessageText?: string;
+	ctaText: string;
+	buttonCaption: string;
+	linkUrl: string;
+	hasTicker: boolean;
+	tickerHeader?: string;
+	signInUrl?: string;
+	secondaryLinkUrl?: string;
+	secondaryLinkLabel?: string;
+	subsLinkUrl?: string;
+}
+export interface EngagementBannerTestParams {
+	titles?: Array<string>;
+	leadSentence?: string;
+	messageText?: string;
+	ctaText?: string;
+	buttonCaption?: string;
+	linkUrl?: string;
+	hasTicker?: boolean;
+	tickerHeader?: string;
+	products?: OphanProduct[];
+	template?: (templateParams: EngagementBannerTemplateParams) => string;
+	bannerModifierClass?: string;
+	minArticlesBeforeShowingBanner?: number;
+	userCohort?: AcquisitionsComponentUserCohort;
+	bannerShownCallback?: () => void;
+}
+
+type ListenerFunction = (f: () => void) => void;
+
+export interface Variant {
+	id: string;
+	test: (x: Object) => void;
+	campaignCode?: string;
+	canRun?: () => boolean;
+	impression?: ListenerFunction;
+	success?: ListenerFunction;
+	engagementBannerParams?: EngagementBannerTestParams;
+}
+
 export interface AbTest {
-	name: string;
+	id: string;
+	start: string;
+	expiry: string;
+	author: string;
+	description: string;
+	audience: number;
+	audienceOffset: number;
+	successMeasure: string;
+	audienceCriteria: string;
+	showForSensitive?: boolean;
+	idealOutcome?: string;
+	dataLinkNames?: string;
+	variants: ReadonlyArray<Variant>;
+	canRun: () => boolean;
+	notInTest?: () => void;
 }
