f05e629b6545efca775f62884a89f95eb6b814f7
Oliver Lloyd <email@oliverlloyd.com>
2020-06-16 15:32:42 +0100

    Added react helpers

--- src/ab.ts
+++ src/ab.ts
@@ -22,6 +22,7 @@ export class AB {
 	private _registerCompleteEvents: OphanAPI['registerCompleteEvents'];
 	private _registerImpressionEvents: OphanAPI['registerImpressionEvents'];
 	private _trackABTests: OphanAPI['trackABTests'];
+
 	constructor(config: ConfigType) {
 		const {
 			mvtMaxValue,
@@ -69,7 +70,6 @@ export class AB {
 	}
 
 	// OphanAPI
-
 	get registerCompleteEvents(): OphanAPI['registerCompleteEvents'] {
 		return this._registerCompleteEvents;
 	}
