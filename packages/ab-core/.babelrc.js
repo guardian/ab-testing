c8df97149d5e738f17039a519142ead1618e3f84
Oliver Lloyd <email@oliverlloyd.com>
2020-06-18 21:28:42 +0100

    Remove react code from core

--- packages/ab-core/.babelrc.js
+++ packages/ab-core/.babelrc.js
@@ -1,9 +1,5 @@
 module.exports = {
-	presets: [
-		'@babel/preset-env',
-		'@babel/preset-typescript',
-		'@babel/preset-react',
-	],
+	presets: ['@babel/preset-env', '@babel/preset-typescript'],
 	env: {
 		test: {
 			plugins: [
