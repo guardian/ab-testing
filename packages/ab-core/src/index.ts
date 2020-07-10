9e9f12df9e43cdc0200c922fe467321187554173
Gareth <gtrufitt@gmail.com>
2020-07-10 17:13:20 +0100

    Remove coreAPI and add back ABTest

--- packages/ab-core/src/index.ts
+++ packages/ab-core/src/index.ts
@@ -1,12 +1,10 @@
 import { AB } from './ab';
 import {
+	ABTest as _ABTest,
 	ABTestAPI as _ABTestAPI,
-	CoreAPI as _CoreAPI,
 	AbTestConfig as _AbTestConfig,
 } from './types';
-
 export { AB };
-
+export declare type ABTest = _ABTest;
 export declare type ABTestAPI = _ABTestAPI;
-export declare type CoreAPI = _CoreAPI;
 export declare type AbTestConfig = _AbTestConfig;
