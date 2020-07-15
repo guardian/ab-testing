46494b525674b34873d8178b2af39bbdf812cdd6
Gareth <gtrufitt@gmail.com>
2020-07-15 16:16:17 +0100

    Add Runnable to exported types

--- packages/ab-core/src/index.ts
+++ packages/ab-core/src/index.ts
@@ -3,8 +3,10 @@ import {
 	ABTest as _ABTest,
 	ABTestAPI as _ABTestAPI,
 	AbTestConfig as _AbTestConfig,
+	Runnable as _Runnable,
 } from './types';
 export { AB };
 export declare type ABTest = _ABTest;
 export declare type ABTestAPI = _ABTestAPI;
 export declare type AbTestConfig = _AbTestConfig;
+export declare type Runnable = _Runnable<_ABTest>;
